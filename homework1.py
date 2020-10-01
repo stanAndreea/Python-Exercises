727f1d140678d793dcd3ee99d03f436ab88de0d0
Andreea Stan <35422422+stanAndreea@users.noreply.github.com>
2020-10-01 16:45:25 +0300

    Update homework1.py

--- homework1.py
+++ homework1.py
@@ -3,3 +3,8 @@ shop = [(x,y) for x in ['shirt',' scarf',' glove',' heat'] for y in ['S','M','L'
 shop.pop()
 shop.remove(('shirt', 'M'))
 shop += [('shirt', 'M')]
+
+#homework ex2 
+tuplex = "h", 10, "o", "m", "e", "w", "o", "r", "k"
+positivion = 1
+tuplex = tuplex[ : positivion ] + tuplex[positivion+1 : ]
